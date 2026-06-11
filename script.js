43108bd8110556f7ab949210560fcdcf200e442e
luizavilar-cell <luiza.vilar@escola.pr.gov.br>
2026-06-11 08:22:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Função interativa do botão
 const btn = document.getElementById('dicaBtn');
-btn.addEventListener('click', () => {
+btn.addEventListener('click', ()=>{
     const mensagem = document.getElementById('mensagem');
     mensagem.textContent = "Dica: Use tecnologia para monitorar plantações e proteger o meio ambiente!";
 });
